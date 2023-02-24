545cf59795a2624a4e084524262bfdef06bab40b
Kingsley Ocran <ocrankingsley@gmail.com>
2023-02-25 00:34:41 +0800

    add task 8

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -3,27 +3,27 @@ const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let bigBrother;
+  let consoleStub;
 
   beforeEach(() => {
-    if (!bigBrother) {
-      bigBrother = sinon.spy(console);
+    if (!consoleStub) {
+      consoleStub = sinon.spy(console);
     }
   });
 
   afterEach(() => {
-    bigBrother.log.resetHistory();
+    consoleStub.log.resetHistory();
   });
 
   it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(consoleStub.log.calledWith('The total is: 120')).to.be.true;
+    expect(consoleStub.log.calledOnce).to.be.true;
   });
 
   it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
     sendPaymentRequestToApi(10, 10);
-    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(consoleStub.log.calledWith('The total is: 20')).to.be.true;
+    expect(consoleStub.log.calledOnce).to.be.true;
   });
 });
